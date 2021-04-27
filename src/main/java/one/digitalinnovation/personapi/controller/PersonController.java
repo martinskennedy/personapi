c6207e3ca399f4b4381b6f0a2645cf226767720f
martinskennedy <kemartins@gmail.com>
2021-04-27 15:49:06 -0300

    Segundo Commit

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,17 +1,31 @@
 package one.digitalinnovation.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import one.digitalinnovation.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 // Classe que é o ponto de entrada do projeto
 
 @RestController // controlador que vai ser acessado por uma API Rest
 @RequestMapping("/api/v1/people")  // caminho principal da API
 public class PersonController {
+    //@GetMapping // requisição da pagina para teste
+    //public String getBook() {return  "API Test!";} // para testar conexão
 
-    @GetMapping // requisição da pagina
-    public String getBook() {
-        return  "API Test!";
+    private PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        return personService.createPerson(person);
     }
 }
